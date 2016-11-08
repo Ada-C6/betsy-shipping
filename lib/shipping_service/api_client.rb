67efe24e2e767c07343b4c69d39a106660f17439
heatherherrington <redherring@gmail.com>
2016-11-07 16:50:27 -0800

    Notes taken in api_client methods_for_order method

--- lib/shipping_service/api_client.rb
+++ lib/shipping_service/api_client.rb
@@ -8,6 +8,22 @@ module ShippingService::APIClient
   ]
 
   def methods_for_order(order)
+    # total_weight in order.rb calculates weight
+    # API gives back shipping options, as in fake data above
+    # Billing zip code needs to be 5 digits - for now, just do that
+    # - weight (pounds)
+    # - origin_country = 'US'
+    # - origin_state = 'WA'
+    # - origin_city = 'Seattle'
+    # - origin_zip = '98102'
+    # - destination_country = 'US'
+    # - destination_state
+    # - destination_city
+    # - destination_zip
+
+    # Probably USPS - based on login information
+
+
     # The real implementation should use the order's
     # shipping details, calculate the weight of every
     # product in the order, and send that info to the API
@@ -20,11 +36,16 @@ module ShippingService::APIClient
   end
 
   def get_method(id)
+    # User chooses which shipping id to implement and sends to API
+    # API sends back delivery and tracking information???
+    # sends shipping_method
+
     # The real implementation should send this ID off to
     # the API and get back the details for a specific
     # shipping method.
     #
     # Instead we'll just return the fake data from above
+
     data = data_for_id(id)
     if data.nil?
       raise ShippingService::ShippingMethodNotFound.new
