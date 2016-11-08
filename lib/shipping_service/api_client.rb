7dc833ff5e6c3f4be56698841c55629f2e2a07c1
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-08 12:27:51 -0800

    Shipping details hash

--- lib/shipping_service/api_client.rb
+++ lib/shipping_service/api_client.rb
@@ -1,5 +1,3 @@
-require 'active_shipping'
-
 module ShippingService::APIClient
 
   FAKE_METHOD_DATA = [
@@ -9,32 +7,35 @@ module ShippingService::APIClient
     {id: 4, name: "FedEx 2 Day", cost: 68.46},
   ]
 
-  BOX_SIZE: [15, 10, 4.5]
+  BOX_SIZE = [15, 10, 4.5]
 
   def methods_for_order(order)
     packages = []
+    weight = 0
     order.orderitems.each do |o|
       product = Product.find(o.product_id)
-      ActiveShipping::Package.new(product.weight * o.quantity * 16, BOX_SIZE, units: :imperial)
-      packages << product
+      weight += product.weight * o.quantity
     end
 
-    origin = ActiveShipping::Location.new(country: 'US', state: 'WA', city: 'Seattle', zip:'98104')
+    weight *= 16
 
-    destination = ActiveShipping::Location.new(country: 'US', state: order.state, city: order.city, zip: order.billing_zip)
+    shipping_details = {}
+    shipping_details[:weight] = weight
+    shipping_details[:box_size] = BOX_SIZE
+    shipping_details[:units] = 'units: :imperial'
 
-    ups = ActiveShipping::UPS.new(login: 'shopifolk', password: 'Shopify_rocks', key: '7CE85DED4C9D07AB')
-    response = ups.find_rates(origin, destination, packages)
+    shipping_details[:origin] = {country: 'US', state: 'WA', city: 'Seattle', zip: '98161'}
 
-    ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    shipping_details[:destination] = {country: 'US', state: order.state, city: order.city, zip: order.billing_zip}
 
-
-    # Check out USPS for comparison...
-    usps = ActiveShipping::USPS.new(login: '677JADED7283')
-    response = usps.find_rates(origin, destination, packages)
-
-    usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
-    # returns an array of usps options
+    # ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    # 
+    # # Check out USPS for comparison...
+    # usps = ActiveShipping::USPS.new(login: '677JADED7283')
+    # response = usps.find_rates(origin, destination, packages)
+    #
+    # usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    # # returns an array of usps options
 
     # # The real implementation should use the order's
     # # shipping details, calculate the weight of every
---
+++ lib/shipping_service_api_wrapper.rb
@@ -0,0 +1,31 @@
+require 'httparty'
+
+class ShippingServiceApiWrapper
+
+  BASE_URL = "localhost:3210"
+
+  #  def self.listrecipes(keyword, page)
+  #    url = BASE_URL + "search?q=#{ keyword }" + "&app_id=#{ TOKEN_ID }" + "&app_key=#{ TOKEN_KEYS }" + "&from=#{ (page.to_i - 1) * 10 }"
+  #    data = HTTParty.get(url)
+  #    recipe_list = []
+  #    if data["count"] != nil && data["count"] > 0
+  #      data["hits"].each do |hit|
+  #        wrapper = Recipe.new(hit["recipe"]["label"], hit["recipe"]["image"], hit["recipe"]["uri"], link: hit["recipe"]["url"], ingredients: hit["recipe"]["ingredients"], dietary_info: hit["recipe"]["dietLabels"], count: data["count"])
+  #        recipe_list << wrapper
+  #      end
+  #    end
+  #    return recipe_list
+  #  end
+   #
+  #  def self.recipe_from_uri(uri)
+  #    url = BASE_URL + "search?r=#{ uri.gsub('#', '%23') }" + "&app_id=#{ TOKEN_ID }" + "&app_key=#{ TOKEN_KEYS }"
+  #    data = HTTParty.get(url)
+  #    recipe_list = []
+  #    if data.count > 0
+  #      recipe = data[0]
+  #      wrapper = Recipe.new(recipe["label"], recipe["image"], recipe["uri"], link: recipe["url"], ingredients: recipe["ingredients"], dietary_info: recipe["dietLabels"], count: 1)
+  #      recipe_list << wrapper
+  #    end
+  #    return recipe_list
+  #  end
+end
