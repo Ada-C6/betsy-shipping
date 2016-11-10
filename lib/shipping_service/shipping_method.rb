910a5bdfa88da1bb7f3292fb33b9584b91cf5f1e
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-10 15:54:20 -0800

    commented out unused items

--- lib/shipping_service/shipping_method.rb
+++ lib/shipping_service/shipping_method.rb
@@ -1,10 +1,10 @@
 class ShippingService::ShippingMethod
 
-  attr_reader :service_name, :cost, :id
+ #  attr_reader :service_name, :cost, :id
 
-	def initialize(details)
-		@service_name = details[:carrier_service]
-    @cost = details[:carrier_rate]
-    @id = details[:service_code]
-  end
+	# def initialize(details)
+	# 	@service_name = details[:carrier_service]
+ #    @cost = details[:carrier_rate]
+ #    @id = details[:service_code]
+ #  end
 end
