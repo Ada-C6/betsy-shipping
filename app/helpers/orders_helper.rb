f6700e084dd69a11ecd7cfc4e99d49395358a708
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-10 14:06:23 -0800

    changed name to service name per the updated ShippingService class

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,5 +1,8 @@
 module OrdersHelper
   def shipping_method_display(method)
-    "#{method.name} - #{number_to_currency(method.cost)}"
+    "#{method.service_name} - #{number_to_currency(method.cost)}"
+  end
+
+  def calculate_weight()
   end
 end
